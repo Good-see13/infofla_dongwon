0cccff256f93125e67a8ad177b1c36f81fc640f9
KOO-96 <rntjdwns1030@naver.com>
2025-11-27 16:41:52 +0900

    추가

--- src/webcam/events/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Event utilities for webcam modules."""
-
-from .gui import start_gui, stop_gui, GUI_ENABLED
-
-__all__ = ["start_gui", "stop_gui", "GUI_ENABLED"]
--- yolo_dataset/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Dongwon > 2025-11-14 1:44pm
-https://universe.roboflow.com/gov24/dongwon-14rhd
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
